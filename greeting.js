4d38f2ed20c28b2e7c3aaa48def2206cfef8931b
csa67 <saiaarathy@gmail.com>
2024-12-31 09:49:08 -0500

    fix

--- greeting.js
+++ greeting.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
